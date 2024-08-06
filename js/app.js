d60d9ca62017058a641c47c537dee8b52419a150
Jaloliddin <hi.shamsiddinov@gmail.com>
2024-08-06 15:51:20 +0500

    finish/hero

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const menuBars = document.getElementById("hamburger");
+const menuContent = document.getElementById("mobileMenu");
+const lies = document.querySelectorAll("#mobileMenu ul li");
+const barsIcon = document.getElementById("barsIcon");
+const xIcon = document.getElementById("xIcon");
+
+menuBars.addEventListener("click", () => {
+  barsIcon.classList.toggle("toggleIcon");
+  xIcon.classList.toggle("toggleIcon");
+  menuContent.classList.toggle("activeMobileMenu");
+  menuContent.style.zIndex = "-1";
+});
+console.log(lies);
+
+lies.forEach((item) => {
+  item.addEventListener("click", () => {
+    barsIcon.classList.toggle("toggleIcon");
+    xIcon.classList.toggle("toggleIcon");
+    menuContent.classList.toggle("activeMobileMenu");
+  });
+});
+// nav action ended
